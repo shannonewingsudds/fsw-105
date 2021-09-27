dedfaca93c2a447370bccdc86877ac1b0ecd4f45
shannon-ewingsudds <shannon.ewing-sudds@bryanuniversity.edu>
2021-09-27 15:38:35 -0400

    Update objects.js

--- objects.js
+++ objects.js
@@ -2,11 +2,14 @@
 //String, number, boolen, function
 //In addition, you should add a groceryCart property to your object, which should be an Array of items that can commonly
 //be found in a grocery cart
+
 var shopper = {
     fruit: "Watermelon",
     vegtable: "Broccoli",
     items: "5",
     shoppingCart: function() {
-        return this.items + " " + this.fruit + " " + this.vegtable;
+        return this.items + " " + this.fruit + " " + this.vegtable
+    console.log("I like to buy" + " " )
     }
+
 };
